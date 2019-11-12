ccb14654385535329e27552c44d0c3ba3cac8371
Seanba <sean@seanba.com>
2019-11-12 00:24:31 -0600

    Push of working example.
    Still need to update to latest ST2U when that is ready.

---
+++ Assets/ExampleFiles/Sprites/MegaDad/OverheadMegaDadController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+namespace MegaDad
+{
+    public class OverheadMegaDadController : MonoBehaviour
+    {
+        private const float MovingSpeedPPS = 0.64f;
+
+        private Vector2 m_Facing = Vector2.down;
+        private Vector2 m_Velocity = Vector2.zero;
+
+        private Animator m_Animator;
+
+        private void Awake()
+        {
+            m_Animator = gameObject.GetComponentInChildren<Animator>();
+            m_Animator.SetFloat("Dir_x", m_Facing.x);
+            m_Animator.SetFloat("Dir_y", m_Facing.y);
+        }
+
+        private void Update()
+        {
+            m_Velocity = Vector2.zero;
+
+            m_Velocity.x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * MovingSpeedPPS;
+            m_Velocity.y = Input.GetAxisRaw("Vertical") * Time.deltaTime * MovingSpeedPPS;
+
+            // Only move one axis at a time, prefering horizontal movement
+            if (m_Velocity.x != 0)
+            {
+                m_Velocity.y = 0;
+            }
+
+            // Remember which way we were facing when we moved
+            if (m_Velocity.SqrMagnitude() != 0)
+            {
+                m_Facing = m_Velocity;
+            }
+
+            gameObject.transform.Translate(m_Velocity);
+        }
+
+        private void LateUpdate()
+        {
+            m_Animator.SetFloat("Dir_x", m_Facing.x);
+            m_Animator.SetFloat("Dir_y", m_Facing.y);
+            m_Animator.SetBool("Moving", m_Velocity.SqrMagnitude() != 0);
+        }
+    }
+}
---
+++ Assets/SuperTiled2Unity/Scripts/SuperMap.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+
+namespace SuperTiled2Unity
+{
+    public class SuperMap : MonoBehaviour
+    {
+        [ReadOnly]
+        public string m_Version;
+
+        [ReadOnly]
+        public string m_TiledVersion;
+
+        [ReadOnly]
+        public MapOrientation m_Orientation;
+
+        [ReadOnly]
+        public MapRenderOrder m_RenderOrder;
+
+        [ReadOnly]
+        public int m_Width;
+
+        [ReadOnly]
+        public int m_Height;
+
+        [ReadOnly]
+        public int m_TileWidth;
+
+        [ReadOnly]
+        public int m_TileHeight;
+
+        [ReadOnly]
+        public int m_HexSideLength;
+
+        [ReadOnly]
+        public StaggerAxis m_StaggerAxis;
+
+        [ReadOnly]
+        public StaggerIndex m_StaggerIndex;
+
+        [ReadOnly]
+        public bool m_Infinite;
+
+        [ReadOnly]
+        public Color m_BackgroundColor;
+
+        [ReadOnly]
+        public int m_NextObjectId;
+
+#if !UNITY_2019_1_OR_NEWER
+        private void Start()
+        {
+            // This is a bad hack but the CompositeCollider2D does not update its geometry in Unity 2018
+            gameObject.SetActive(false);
+            gameObject.SetActive(true);
+
+            foreach (var collider in GetComponentsInChildren<CompositeCollider2D>())
+            {
+                collider.GenerateGeometry();
+            }
+        }
+#endif
+
+        public Vector3Int TiledIndexToGridCell(int index, int offset_x, int offset_y, int stride)
+        {
+            int x = index % stride;
+            int y = index / stride;
+            x += offset_x;
+            y += offset_y;
+
+            var pos3 = TiledCellToGridCell(x, y);
+            pos3.y = -pos3.y;
+
+            return pos3;
+        }
+
+        private Vector3Int TiledCellToGridCell(int x, int y)
+        {
+            if (m_Orientation == MapOrientation.Isometric)
+            {
+                return new Vector3Int(-y, x, 0);
+            }
+            else if (m_Orientation == MapOrientation.Staggered)
+            {
+                var isStaggerX = m_StaggerAxis == StaggerAxis.X;
+                var isStaggerOdd = m_StaggerIndex == StaggerIndex.Odd;
+
+                if (isStaggerX)
+                {
+                    var pos = new Vector3Int(x - y, x + y, 0);
+
+                    if (isStaggerOdd)
+                    {
+                        pos.x -= (x + 1) / 2;
+                        pos.y -= x / 2;
+                    }
+                    else
+                    {
+                        pos.x -= x / 2;
+                        pos.y -= (x + 1) / 2;
+                    }
+
+                    return pos;
+                }
+                else
+                {
+                    var pos = new Vector3Int(x, y + x, 0);
+
+                    if (isStaggerOdd)
+                    {
+                        var stagger = y / 2;
+                        pos.x -= stagger;
+                        pos.y -= stagger;
+                    }
+                    else
+                    {
+                        var stagger = (y + 1) / 2;
+                        pos.x -= stagger;
+                        pos.y -= stagger;
+                    }
+
+                    return pos;
+                }
+            }
+            else if (m_Orientation == MapOrientation.Hexagonal)
+            {
+                var isStaggerX = m_StaggerAxis == StaggerAxis.X;
+                var isStaggerOdd = m_StaggerIndex == StaggerIndex.Odd;
+
+                if (isStaggerX)
+                {
+                    // Flat top hex
+                    if (isStaggerOdd)
+                    {
+                        return new Vector3Int(-y, -x - 1, 0);
+                    }
+                    else
+                    {
+                        return new Vector3Int(-y, -x, 0);
+                    }
+                }
+                else
+                {
+                    // Pointy top hex
+                    if (isStaggerOdd)
+                    {
+                        return new Vector3Int(x, y, 0);
+                    }
+                    else
+                    {
+                        return new Vector3Int(x, y + 1, 0);
+                    }
+                }
+            }
+
+            // Simple maps (like orthongal do not transform indices into other spaces)
+            return new Vector3Int(x, y, 0);
+        }
+    }
+}
